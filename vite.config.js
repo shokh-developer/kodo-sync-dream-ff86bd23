70e132ee890c9123ae9381cf321fc5aa94b3bee5
Shohjahon <shohjaxontursunov50@gmail.com>
2026-01-23 20:53:35 +0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    host: true, // Render serverda hamma xostlar ruxsat
-    port: 3000  // Render odatda portni ENV orqali beradi
-  }
-})
