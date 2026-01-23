7bff49ac8bcf6bbfb65b65d8a3164e061eceefc1
Shohjahon <shohjaxontursunov50@gmail.com>
2026-01-23 20:59:18 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// Render.com uchun tayyor vite.config.js
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: true, // hamma xostlar uchun ruxsat
-    port: process.env.PORT ? Number(process.env.PORT) : 5173, // Render portni env orqali beradi
-    hmr: {
-      overlay: false, // development warning overlay’ini o‘chiradi
-    },
+server: {
+  host: true, // hamma xostlarga ruxsat
+  port: process.env.PORT ? Number(process.env.PORT) : 4173, // Render portni env orqali oladi
+  hmr: {
+    overlay: false,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger() // faqat dev mode uchun
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // @ -> src papkasi
-    },
-  },
-  build: {
-    outDir: "dist", // production build papkasi
-  },
-}));
+}
